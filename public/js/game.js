96264d6707302fb75d5d560093e32e4ef0694f94
pbolinao <pdbolinao@gmail.com>
2020-10-29 21:58:04 -0700

    non-functional game gui

---
+++ public/js/game.js
@@ -0,0 +1,68 @@
+const   defStartP1 = ["a1", "a2", "a3", "a4", "a5", 
+                    "b1", "b2", "b3", "b4", "b5", "b6",
+                    "c3", "c4", "c5"],    // Array of coordinates for PLAYER 1's default layout
+        defStartP2 = ["i5", "i6", "i7", "i8", "i9", 
+                    "h4", "h5", "h6", "h7", "h8", "h9",
+                    "g5", "g6", "g7"],    // Array of coordinates for PLAYER 2's default layout
+        BDStartP1 = ["i8", "i9", "h7", "h8", "h9", "g7", "g8",
+                    "c2", "c3", "b1", "b2", "b3", "a1", "a2"],     // Array of coordinates for PLAYER 1's  Belgium Daisy layout
+        BDStartP2 = ["i5", "i6", "h4", "h5", "h6", "g4", "g5",
+                    "c5", "c6", "b4", "b5", "b6", "a4", "a5"],     // Array of coordinates for PLAYER 2's Belgium Daisy layout
+        GDStartP1 = ["h8", "h9", "g7", "g8", "g9", "f7", "f8",
+                    "b1", "b2", "c1", "c2", "c3", "d2", "d3"],     // Array of coordinates for PLAYER 1's German Daisy layout
+        GDStartP2 = ["h4", "h5", "g3", "g4", "g5", "f3", "f4",
+                    "b5", "b6", "c5", "c6", "c7", "d6", "d7"];     // Array of coordinates for PLAYER 2's German Daisy layout
+
+let marblesP1 = [],
+    marblesP2 = [],
+    themeNo = 0;  
+
+// window.onload = function() {
+//     initBoard();
+// }
+
+function initBoard(startStyle) {
+    // 0 or undefined = default
+    // 1 = Belgium Daisy
+    // 2 = German Daisy
+    let startCoordsP1 = [],
+        startCoordsP2 = [];
+    if (!startStyle) {
+        console.log("Default layout")
+        startCoordsP1 = defStartP1;
+        startCoordsP2 = defStartP2;
+    } else if (startStyle == 1) {
+        console.log("Belgium Daisy");
+        startCoordsP1 = BDStartP1;
+        startCoordsP2 = BDStartP2;
+    } else if (startStyle == 2) {
+        console.log("German Daisy");
+        startCoordsP1 = GDStartP1;
+        startCoordsP2 = GDStartP2;
+    }
+
+    for (i = 0; i < startCoordsP1.length; i++) {
+        document.getElementById(startCoordsP1[i]).style.background = blackMarbleColour;
+        // create a black marble object then add to marblesP1 array
+        document.getElementById(startCoordsP2[i]).style.background = whiteMarbleColour;
+        // create a white marble object then add to marblesP2 array
+    }
+}
+
+function changeTheme() {
+    themeNo++;
+    if (themeNo > 1) {
+        themeNo = 0;
+    }
+    let gameboard = document.getElementById("game");
+    if (themeNo == 0) {
+        gameboard.style.background = "url(../images/abba-transparent.png)";
+        document.getElementById("game-title").innerHTML = "ABBA-Lone"
+    } else if (themeNo == 1) {
+        gameboard.style.background = "url(../images/jabba.png)";
+        document.getElementById("game-title").innerHTML = "Jabba-Lone"
+    }
+    gameboard.style.backgroundSize = "contain";
+    gameboard.style.backgroundRepeat = "no-repeat";
+    gameboard.style.backgroundPosition = "center";
+}
---
+++ public/js/strings.js
@@ -0,0 +1,3 @@
+const   emptyCellColour = "radial-gradient(circle at 12px 12px, #b3b3b3e0, #000000e0)",
+        blackMarbleColour = "radial-gradient(circle at 12px 12px, #a5a5a5f1, #000000f1, #000000f1)",
+        whiteMarbleColour = "radial-gradient(circle at 12px 12px,  #fffffff1, #fffffff1, #8d8d8df1, #505050f1)";
