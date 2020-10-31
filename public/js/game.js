aa6f9afacf56091817337b2a101a7a8a6ae8d416
pbolinao <pdbolinao@gmail.com>
2020-10-31 15:39:05 -0700

    added history tabs

--- public/js/game.js
+++ public/js/game.js
@@ -1,17 +1,15 @@
-const   defStartP1 = ["a1", "a2", "a3", "a4", "a5", 
-                    "b1", "b2", "b3", "b4", "b5", "b6",
-                    "c3", "c4", "c5"],    // Array of coordinates for PLAYER 1's default layout
-        defStartP2 = ["i5", "i6", "i7", "i8", "i9", 
-                    "h4", "h5", "h6", "h7", "h8", "h9",
-                    "g5", "g6", "g7"],    // Array of coordinates for PLAYER 2's default layout
+const   defStartP1 = ["a1", "a2", "a3", "a4", "a5", "b1", "b2", 
+                    "b3", "b4", "b5", "b6", "c3", "c4", "c5"],      // Array of coordinates for PLAYER 1's default layout
+        defStartP2 = ["i5", "i6", "i7", "i8", "i9", "h4", "h5",
+                    "h6", "h7", "h8", "h9", "g5", "g6", "g7"],      // Array of coordinates for PLAYER 2's default layout
         BDStartP1 = ["i8", "i9", "h7", "h8", "h9", "g7", "g8",
-                    "c2", "c3", "b1", "b2", "b3", "a1", "a2"],     // Array of coordinates for PLAYER 1's  Belgium Daisy layout
+                    "c2", "c3", "b1", "b2", "b3", "a1", "a2"],      // Array of coordinates for PLAYER 1's  Belgium Daisy layout
         BDStartP2 = ["i5", "i6", "h4", "h5", "h6", "g4", "g5",
-                    "c5", "c6", "b4", "b5", "b6", "a4", "a5"],     // Array of coordinates for PLAYER 2's Belgium Daisy layout
+                    "c5", "c6", "b4", "b5", "b6", "a4", "a5"],      // Array of coordinates for PLAYER 2's Belgium Daisy layout
         GDStartP1 = ["h8", "h9", "g7", "g8", "g9", "f7", "f8",
-                    "b1", "b2", "c1", "c2", "c3", "d2", "d3"],     // Array of coordinates for PLAYER 1's German Daisy layout
+                    "b1", "b2", "c1", "c2", "c3", "d2", "d3"],      // Array of coordinates for PLAYER 1's German Daisy layout
         GDStartP2 = ["h4", "h5", "g3", "g4", "g5", "f3", "f4",
-                    "b5", "b6", "c5", "c6", "c7", "d6", "d7"];     // Array of coordinates for PLAYER 2's German Daisy layout
+                    "b5", "b6", "c5", "c6", "c7", "d6", "d7"];      // Array of coordinates for PLAYER 2's German Daisy layout
 
 let marblesP1 = [],
     marblesP2 = [],
@@ -65,4 +63,26 @@ function changeTheme() {
     gameboard.style.backgroundSize = "contain";
     gameboard.style.backgroundRepeat = "no-repeat";
     gameboard.style.backgroundPosition = "center";
+}
+
+function toggleHistory(ghTab) {
+    let fhBTN = document.getElementById("gh-tabs-fh"),
+        aitBTN = document.getElementById("gh-tabs-ai"),
+        clickedBTN = null,
+        notClickedBTN = null;
+    if (!ghTab) {
+        document.getElementById("gh-full").style.display = "block";
+        document.getElementById("gh-ai-time").style.display = "none";
+        clickedBTN = fhBTN;
+        notClickedBTN = aitBTN;
+    } else if (ghTab == 1) {
+        document.getElementById("gh-ai-time").style.display = "block";
+        document.getElementById("gh-full").style.display = "none";
+        clickedBTN = aitBTN;
+        notClickedBTN = fhBTN;
+    }
+    if (!clickedBTN.classList.contains("selected-btn")) {
+        clickedBTN.classList.add("selected-btn");
+        notClickedBTN.classList.remove("selected-btn");
+    }
 }
